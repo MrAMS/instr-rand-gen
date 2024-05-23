32e5d237d6cf835f1846b08f1bab37bb46f87895
Santiego <2421653893@qq.com>
2024-05-23 16:49:40 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,87 +2,51 @@ use std::io::Write;
 
 use rand::Rng;
 
-#[derive(Debug, Clone)]
-struct InstrPatternPart {
-    value: Vec<bool>,
-    width: usize,
-    unspcified: bool,
-}
-
-impl InstrPatternPart{
-    pub fn clear(&mut self){
-        self.value.clear();
-        self.width = 0;
-        self.unspcified = true;
-    }
+#[derive(Debug, Copy, Clone)]
+enum InstrPatternPart{
+    One, Zero, DontCare,
 }
 
 struct InstrPattern{
-    patterns: Vec<InstrPatternPart>,
+    name: &'static str,
+    parts: Vec<InstrPatternPart>,
 }
 
 impl InstrPattern{
-    pub fn new(pattern: &str) -> Self{
-        let mut patterns: Vec<InstrPatternPart> = Vec::new();
-        let mut nxt_part = InstrPatternPart{
-            value: Vec::new(),
-            width: 0,
-            unspcified: false,
-        };
+    pub fn new(name: &'static str, pattern: &str) -> Self{
+        let mut parts: Vec<InstrPatternPart> = Vec::new();
         for c in pattern.chars().rev(){
-            let unspcified: bool;
             match c {
-                '1' | '0' => {
-                    unspcified = false;
+                '1' => {
+                    parts.push(InstrPatternPart::One);
+                },
+                '0' => {
+                    parts.push(InstrPatternPart::Zero);
                 },
                 '?' => {
-                    unspcified = true;
+                    parts.push(InstrPatternPart::DontCare);
                 }
                 ' ' => continue,
-                _ => panic!("[Error] pattern must only contains 1, 0 or ?"),
+                _ => panic!("[Error] find {}, pattern must only contains 1, 0 or ?", c),
             };
-            if nxt_part.width == 0 {
-                nxt_part.unspcified = unspcified;
-            }else if nxt_part.unspcified != unspcified {
-                patterns.push(nxt_part.clone());
-                nxt_part.clear();
-                nxt_part.unspcified = unspcified;
-            }
-            nxt_part.width += 1;
-            if !unspcified {
-                nxt_part.value.push(c == '1');
-            }
-        }
-        patterns.push(nxt_part);
-        InstrPattern{
-            patterns
-        }
-    }
-    pub fn generate(&self) -> Self{
-        let mut patterns = self.patterns.clone();
-        for part in patterns.iter_mut(){
-            if part.unspcified{
-                for _ in 0..part.width{
-                    part.value.push(rand::random::<bool>());
-                }
-            }
         }
         InstrPattern{
-            patterns
+            name,
+            parts
         }
     }
-    pub fn write_bits<T: std::ops::BitOr<Output = T> + std::ops::BitXor<Output = T> + std::ops::Shl<Output = T> + std::convert::From<u8> + Default>(&self) -> T {
+    pub fn generate<T: std::ops::BitOrAssign + std::ops::Shl<Output = T> + std::convert::From<u8> + Default>(&self) -> T {
         let mut bits = T::default();
         // bits = bits ^ bits;
-        let mut i: u8 = 0;
         let bits_size = std::mem::size_of::<T>()*8;
-        for part in self.patterns.iter(){
-            for bit in part.value.iter(){
-                assert!(i <= u8::try_from(bits_size).unwrap(), "[Error] pattern is too long for this type");
-                if *bit {
-                    bits = bits | T::from(1) << T::from(i);
-                }
-                i += 1;
+        assert!(self.parts.len() <= bits_size, "[Error] pattern is too long for this type");
+        for i in 0..self.parts.len() {
+            let part = self.parts[i];
+            let i_u8 = u8::try_from(i).unwrap();
+            match part {
+                InstrPatternPart::One => bits |= T::from(1) << T::from(i_u8),
+                InstrPatternPart::Zero => (),
+                InstrPatternPart::DontCare => bits |= T::from(if rand::random::<bool>(){1} else{0}) << T::from(i_u8),
             }
         }
         bits
@@ -93,23 +57,23 @@ fn main() {
     let instrs_num = 10;
     let output_path = std::path::Path::new("test.bin");
     let patterns = vec![
-        "00000000000100000???????????????", // ADDW
-        "00000000000100010???????????????", // SUBW
-        "00000000000100100???????????????", // SLT
-        "00000000000100101???????????????", // SLTU
+        ["00000000000100000???????????????", "ADDW"],
+        ["00000000000100010???????????????", "SUBW"], 
+        ["00000000000100100???????????????", "SLT"], 
+        ["00000000000100101???????????????", "SLTU"],
     ];
 
     let mut seeds = Vec::new();
     for p in  patterns {
-        seeds.push(InstrPattern::new(p));
+        seeds.push(InstrPattern::new(p[1], p[0]));
     }
 
     let mut f = std::fs::File::create(&output_path).unwrap();
     let mut rng = rand::thread_rng();
     for _ in 1..instrs_num{
         let i = rng.gen_range(0..seeds.len());
-        let instr = seeds[i].generate().write_bits::<u32>();
-        println!("{:#b}", instr);
+        let instr = seeds[i].generate::<u32>();
+        println!("{} {:#b}", seeds[i].name, instr);
         f.write_all(&instr.to_le_bytes()).unwrap();
     }
 }
