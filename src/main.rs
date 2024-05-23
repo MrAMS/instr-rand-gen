deaac5b365d77fb87c1d467edb7c62abb402c806
Santiego <2421653893@qq.com>
2024-05-23 17:31:23 +0800

    feat: can check if a pattern match bits

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ enum InstrPatternPart{
     One, Zero, DontCare,
 }
 
-struct InstrPattern{
-    name: &'static str,
+struct InstrPattern<IdType>{
+    id: IdType,
     parts: Vec<InstrPatternPart>,
 }
 
-impl InstrPattern{
-    pub fn new(name: &'static str, pattern: &str) -> Self{
+impl<IdType> InstrPattern<IdType>{
+    pub fn new(id: IdType, pattern: &str) -> Self{
         let mut parts: Vec<InstrPatternPart> = Vec::new();
         for c in pattern.chars().rev(){
             match c {
@@ -31,7 +31,7 @@ impl InstrPattern{
             };
         }
         InstrPattern{
-            name,
+            id,
             parts
         }
     }
@@ -51,17 +51,51 @@ impl InstrPattern{
         }
         bits
     }
+    pub fn fit<T: std::ops::BitOrAssign + std::ops::Shr<Output = T> + std::ops::BitAnd<Output = T> + std::cmp::PartialEq + std::convert::From<u8> + Default + Copy>(&self, bits: T) -> bool {
+        let one = T::from(1);
+        let zero = T::from(0);
+        if std::mem::size_of::<T>()*8 < self.parts.len() {
+            return false;
+        }
+        for (i, part) in self.parts.iter().enumerate() {
+            let i_u8 = u8::try_from(i).unwrap();
+            match part {
+                InstrPatternPart::One => {
+                    if (bits >> T::from(i_u8)) & one != one {
+                        return false;
+                    }
+                },
+                InstrPatternPart::Zero => {
+                    if (bits >> T::from(i_u8)) & one != zero {
+                        return false;
+                    }
+                },
+                InstrPatternPart::DontCare => (),
+            }
+        }
+        true
+    }
 }
 
 fn main() {
     let instrs_num = 10;
     let output_path = std::path::Path::new("test.bin");
     let patterns = vec![
+        // MSB
         ["00000000000100000???????????????", "ADDW"],
         ["00000000000100010???????????????", "SUBW"], 
         ["00000000000100100???????????????", "SLT"], 
         ["00000000000100101???????????????", "SLTU"],
+        ["00000000000101000???????????????", "NOR"],
+        ["00000000000101001???????????????", "AND"],
+        ["00000000000101010???????????????", "OR"],
+        ["00000000000101011???????????????", "XOR"],
+        ["00000000000101110???????????????", "SLLW"],
+        ["00000000000101111???????????????", "SRLW"],
+        ["00000000000110000???????????????", "SRAW"],
     ];
+
+    assert!(InstrPattern::new("ADDW", "00000000000100000???????????????").fit(0x0010421c));
 
     let mut seeds = Vec::new();
     for p in  patterns {
@@ -73,7 +107,7 @@ fn main() {
     for _ in 1..instrs_num{
         let i = rng.gen_range(0..seeds.len());
         let instr = seeds[i].generate::<u32>();
-        println!("{} {:#b}", seeds[i].name, instr);
+        println!("{} {:#b}", seeds[i].id, instr);
         f.write_all(&instr.to_le_bytes()).unwrap();
     }
 }
